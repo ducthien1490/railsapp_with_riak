4e21c753a9d2d8cc1283bef22aa217664db1226c
Duc Thien <ducthien1490@gmail.com>
2014-10-24 18:47:31 +0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiakExample::Application.config.secret_token = 'e849cd42af8510377856877e0baf48907f092e4737248f08dfd520411d7186fc111cf1bfbae6b44a23c150449a7b6b6d3c4b836ac488741964ff30cf16119aa7'
